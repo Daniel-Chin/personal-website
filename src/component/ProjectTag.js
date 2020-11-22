764f4e8c7c57cc3fd032c183320bd274a8028f53
Daniel Chin <daniel_chin@yahoo.com>
2020-11-22 20:42:28 +0800

    balanced positive negatieve tages

--- src/component/ProjectTag.js
+++ src/component/ProjectTag.js
@@ -6,8 +6,16 @@ const ProjectTag = ({
   set_positive_tags, set_negative_tags,
 }) => {
   const [ expanded, set_expanded ] = useState(false);
-  const highlight = filterable && positive_tags.includes(tag);
+  const is_positive = filterable && positive_tags.includes(tag);
+  const is_negative = filterable && negative_tags.includes(tag);
+  let color = 'white';
   let caption = tag;
+  if (is_positive) {
+    color = '#ff0';
+  }
+  if (is_negative) {
+    color = '#f00';
+  }
   
   const clickTag = (_, force) => {
     if (filterable) {
@@ -22,14 +30,18 @@ const ProjectTag = ({
     }
   }
   const clickPositive = () => {
-    if (highlight) {
+    if (is_positive) {
       set_positive_tags(positive_tags.filter((t) => (t !== tag)));
     } else {
       set_positive_tags([ ...positive_tags, tag ]);
     }
   };
   const clickNegative = () => {
-    set_negative_tags([ ...negative_tags, tag ]);
+    if (is_negative) {
+      set_negative_tags(negative_tags.filter((t) => (t !== tag)));
+    } else {
+      set_negative_tags([ ...negative_tags, tag ]);
+    }
   };
 
   let title = null;
@@ -57,8 +69,8 @@ const ProjectTag = ({
             cursor: expanded ? 'text' : (
               filterable ? 'pointer' : 'default'
             ),
-            color: highlight ? '#ff0' : 'white', 
-            fontWeight: highlight ? 'bold' : 'normal',
+            color, 
+            fontWeight: is_positive ? 'bold' : 'normal',
           }} onClick={clickTag} title={title} 
           tabIndex={0} onKeyUp={(event) => {
             if (event.key === 'Enter' || event.keyCode === 13) {
@@ -68,17 +80,19 @@ const ProjectTag = ({
         > 
           {caption}
         </span>
-        <span className='tag-positive smol-pad' 
-          hidden={! expanded} onClick={clickPositive}
-          tabIndex={0} onKeyUp={(event) => {
-            if (event.key === 'Enter' || event.keyCode === 13) {
-              clickPositive();
-            }
-          }}
-        >
-          {highlight ? 'unfilter' : 'filter'}
-        </span>
-        {highlight ? null :
+        {is_negative ? null :
+          <span className='tag-positive smol-pad' 
+            hidden={! expanded} onClick={clickPositive}
+            tabIndex={0} onKeyUp={(event) => {
+              if (event.key === 'Enter' || event.keyCode === 13) {
+                clickPositive();
+              }
+            }}
+          >
+            {is_positive ? 'unfilter' : 'filter'}
+          </span>
+        }
+        {is_positive ? null :
           <span className='tag-negative smol-pad' 
             hidden={! expanded} onClick={clickNegative}
             tabIndex={0} onKeyUp={(event) => {
@@ -87,7 +101,7 @@ const ProjectTag = ({
               }
             }}
             >
-            exclude
+            {is_negative ? 'unexclude' : 'exclude'}
           </span>
         }
       </span>
