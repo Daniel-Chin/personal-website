1d8c0f563bb5bf251e5e77bbe2fcc845ee47e808
Daniel Chin <daniel_chin@yahoo.com>
2020-11-22 18:15:08 +0800

    tag element wings

--- src/component/ProjectTag.js
+++ src/component/ProjectTag.js
@@ -1,12 +1,33 @@
 import React, { useState } from 'react';
 
-const ProjectTag = ({ tag }) => {
+const ProjectTag = ({ tag, filterable }) => {
   const [ expanded, set_expanded ] = useState(false);
   
+  const clickTag = () => {
+    if (filterable && ! expanded) {
+      set_expanded(true);
+    }
+  };
+  const onMouseLeave = () => {
+    if (expanded) {
+      set_expanded(false);
+    }
+  }
+
   return (
     <>
-      <span className='tag-span'>
-        {tag}
+      <span className='tag-span' onMouseLeave={onMouseLeave}>
+        <span className='tag-middle smol-pad' style={{
+          cursor: expanded ? 'text' : 'pointer',
+        }} onClick={clickTag}>
+          {tag}
+        </span>
+        <span className='tag-positive smol-pad' hidden={! expanded}>
+          filter
+        </span>
+        <span className='tag-negative smol-pad' hidden={! expanded}>
+          exclude
+        </span>
       </span>
       <span>{' '}</span>
     </>
