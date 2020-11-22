2becc7226f4f35f886c6658aab0940e385b6f8ff
Daniel Chin <daniel_chin@yahoo.com>
2020-11-22 18:23:50 +0800

    tag tooltip

--- src/component/ProjectTag.js
+++ src/component/ProjectTag.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 
-const ProjectTag = ({ tag, filterable }) => {
+const ProjectTag = ({ tag, year, pride, filterable }) => {
   const [ expanded, set_expanded ] = useState(false);
+  let caption = tag;
   
   const clickTag = () => {
     if (filterable && ! expanded) {
@@ -13,19 +14,48 @@ const ProjectTag = ({ tag, filterable }) => {
       set_expanded(false);
     }
   }
+  const clickPositive = () => {
+
+  };
+  const clickNegative = () => {
+
+  };
+
+  let title = null;
+  if (year) {
+    title = `Age ${tag - 1999}`;
+  }
+  if (pride) {
+    caption = `Pride=${tag}`;
+    title = [
+      '0',
+      'Ignore this plz',
+      'Useful but trivial',
+      'Hmm...',
+      'shows expertise, but is not eye-catching',
+      'shows who I am',
+      'Check this out!',
+    ][tag];
+  }
 
   return (
     <>
       <span className='tag-span' onMouseLeave={onMouseLeave}>
         <span className='tag-middle smol-pad' style={{
-          cursor: expanded ? 'text' : 'pointer',
-        }} onClick={clickTag}>
-          {tag}
+          cursor: expanded ? 'text' : (
+            filterable ? 'pointer' : 'default'
+          ),
+        }} onClick={clickTag} title={title}>
+          {caption}
         </span>
-        <span className='tag-positive smol-pad' hidden={! expanded}>
+        <span className='tag-positive smol-pad' 
+          hidden={! expanded} onClick={clickPositive}
+        >
           filter
         </span>
-        <span className='tag-negative smol-pad' hidden={! expanded}>
+        <span className='tag-negative smol-pad' 
+          hidden={! expanded} onClick={clickNegative}
+        >
           exclude
         </span>
       </span>
