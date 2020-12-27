9bffb9373e4ba89a3bbe2b6b9e27a6c2307c06da
Daniel Chin <daniel_chin@yahoo.com>
2020-12-27 20:35:27 +0800

    smart iframe that works

--- src/component/SmartIframe.js
+++ src/component/SmartIframe.js
@@ -1,47 +1,51 @@
 import React, {
-  createRef, useState, useLayoutEffect, 
-  useCallback
+  createRef, useState, useLayoutEffect, useEffect, 
+  useCallback, 
 } from 'react';
 
 const SmartIframe = ({ target, title }) => {
   const iFrame = createRef();
-  const [iframeHeight, setIframeHeight] = useState(10);
+  const [iframeHeight, setIframeHeight] = useState(100);
   const [src, setSrc] = useState('');
   const [shrunk, setShrunk] = useState(true);
   
-  const resizeIframe = useCallback(() => {
+  const shrink = () => {
+    setIframeHeight(50);
+    setShrunk(true);
+    // console.log('shrink');
+  };
+  
+  const expand = useCallback(() => {
     setIframeHeight(
       iFrame.current.contentWindow.document.body
-      .scrollHeight + 80
+      .scrollHeight + 0
     );
-  }, [iFrame]);
+    setShrunk(false);
+    // console.log('expand', iFrame.current.contentWindow.document.body.scrollHeight);
+}, [iFrame]);
   
   useLayoutEffect(() => {
     if (shrunk) {
-      setShrunk(false);
-      resizeIframe();
-      console.log('unshrink');
+      expand();
     }
-    const onWindowResize = () => {
-      setIframeHeight(10);
-      setShrunk(true);
-    };
-    window.addEventListener('resize', onWindowResize);
-    return () => window.removeEventListener('resize', onWindowResize);
-  }, [resizeIframe, shrunk]);
+    window.addEventListener('resize', shrink);
+    return () => window.removeEventListener('resize', shrink);
+  }, [expand, shrunk]);
   
-  useLayoutEffect(() => {
-    setIframeHeight(10);
-    setShrunk(true);
+  useEffect(() => {
     setSrc(target);
-    console.log('target changed');
   }, [target]);
   
+  const onLoad = () => {
+    // console.log('onLoad');
+    shrink();
+  };
+
   return (
     <iframe 
       ref={iFrame} src={src}
       title={title} className='smart-iframe'
-      onLoad={resizeIframe}
+      onLoad={onLoad}
       style={{ height: `${iframeHeight}px` }}
     />
 );
