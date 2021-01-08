e981a23f77939c49baad62152c555817ad3b6540
Daniel Chin <daniel_chin@yahoo.com>
2021-01-08 20:41:37 +0800

    trying to use nodejs to print md to html. Note: build.py modification accidentally added to the last commit 6a942f20b21e1cd5639142a53f9d0bca28303c3a

---
+++ public/blog/md.js
@@ -0,0 +1,16 @@
+const { readFileSync, writeFileSync } = require('fs');
+var hljs = require('highlight.js');
+const md = require('markdown-it')({
+  highlight: (str, lang) => {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str).value;
+      } catch (__) {}
+    }
+    return ''; // use external default escaping
+  }, 
+});
+
+const src = readFileSync('md_js_in.md', {encoding: 'utf-8'});
+const body = md.render(src);
+writeFileSync('md_js_out.body', body, {encoding: 'utf-8'});
