a5adf7177ccff7ccc591e98e56759bdbde2c9076
Daniel Chin <daniel.chin@nyu.edu>
2024-10-07 16:47:10 +0400

    drop public key auth

--- src/page/WhoamiPage.js
+++ src/page/WhoamiPage.js
@@ -17,20 +17,7 @@ const WhoamiPage = () => {
           https://github.com/daniel-chin
         </a>. 
         <br />
-        I put a public key there. You can verify that 
-        your current HTTPS connection uses the same 
-        public key. 
-      </p>
-      <p>
-        If it actually does not, Netlify may have changed 
-        key without asking me. 
-        In that case, please open an issue at {' '}
-        <a 
-          href="https://github.com/Daniel-Chin/daniel-chin/issues"
-          rel='noreferrer' target='_blank'
-        >
-          https://github.com/Daniel-Chin/daniel-chin/issues
-        </a> {' '} if you will. 
+        You can see a URL pointing back to here. 
       </p>
     </div>
   );
